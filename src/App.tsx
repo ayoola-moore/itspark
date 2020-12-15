667bbcce4d86ad1c3b833ec1d37a47621ad8394b
hayoola <onaolapo.ayo@gmail.com>
2020-12-15 10:59:57 +0400

    initial set up: typescript, formik, yup etc

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import InputForm from "./InputForm";
+
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
