960d1d72e114d47031d6e2a01cfb4ed40a8bd66b
hayoola <onaolapo.ayo@gmail.com>
2020-12-15 23:14:25 +0400

    Updates

---
+++ precache-manifest.c1b1cedd03f4b9936bca0169e08d412e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca018bd17d105ed2c998934f2aee365c",
+    "url": "/itspark/index.html"
+  },
+  {
+    "revision": "c0cf3c854591bd5a1ec9",
+    "url": "/itspark/static/css/main.715422c2.chunk.css"
+  },
+  {
+    "revision": "2265bbf7fa1c07decbb0",
+    "url": "/itspark/static/js/2.bf6fa449.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/itspark/static/js/2.bf6fa449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cf3c854591bd5a1ec9",
+    "url": "/itspark/static/js/main.b981903f.chunk.js"
+  },
+  {
+    "revision": "e14408a95a4ad2f6eb04",
+    "url": "/itspark/static/js/runtime-main.ac49c588.js"
+  }
+]);
